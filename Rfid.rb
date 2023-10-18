acd69fe4d128661686096f54ad073e34da369703
JoanGuardia <145378547+JoanGuardiaA@users.noreply.github.com>
2023-10-18 08:54:50 +0200

    Update Rfid.rb

--- Rfid.rb
+++ Rfid.rb
@@ -1,13 +1,11 @@
 require 'ruby-nfc'
 class Rfid
-	def initialize 
-		@@readers = NFC::Reader.all
-	end
-     #return uid in hexa str
-    def read_uid
-			@@readers[0].poll(Mifare::Classic::Tag) do |tag|
-			uid = tag.uid_hex.upcase
-			return uid
+	@@readers = NFC::Reader.all
+    	#return uid in hexa str
+   	def read_uid
+		@@readers[0].poll(Mifare::Classic::Tag) do |tag|
+		uid = tag.uid_hex.upcase
+		return uid
 		end
 	end
 end
